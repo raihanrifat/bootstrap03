d433adbbe63c52087050971702bc2b168f251119
DESKTOP-UFFBBJ6\RIFAT <rifatraihan000@gmail.com>
2017-07-08 22:22:02 +0600

    project 3 uploaded

---
+++ js/plugin.js
@@ -0,0 +1,34 @@
+(function(){
+     var method;
+    var noop = function(){};
+    var methods = [
+        'assert','clear','count','debug','dir','dirxml','error','exception','group','groupCollapsed','groupend','info','log','markTimeline','profile','profileEnd','table','time','timeEnd','timeline','timelineEnd','timeStamp','trace','warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+    while (length--){
+        method= methods[length];
+        if(!console[method]){
+            console[method] = noop;
+     }
+    }
+    $('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
+}());
